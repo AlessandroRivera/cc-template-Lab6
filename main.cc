a7892e304d14fa7763a7bce5c10f2e7093ac955e
Alessandro <122322492+AlessandroRivera@users.noreply.github.com>
2024-09-20 10:53:45 -0600

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stack>  // Libreria para pilas
+#include <queue>  // Libreria para colas
+
+int main(int argc, char *argv[]) {
+
+  // Ingresar codigo aqui
+
+    return 0;
+}
